3e6133ad50c0496f2ab93fe5f34ebed3434d01e8
Vaibhav-Wachkal <vwachkal95@gmail.com>
2021-03-25 10:12:54 +0530

    done

--- src/index.js
+++ src/index.js
@@ -15,10 +15,12 @@ const app=express();
 const port=process.env.PORT
 
 app.use(express.json())
-app.use(userRouter)
-app.use(storyRouter)
-
+//app.use(userRouter)
+//app.use(storyRouter)
+app.use('/api', cors(corsOptionsDelegate), userRouter);
+app.use('/api', cors(corsOptionsDelegate), storyRouter);
 
 app.listen(port,()=>{
     console.log("server is up on port "+port);
 })
+
